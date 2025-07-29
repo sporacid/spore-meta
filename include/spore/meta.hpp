ca1a353bba7cb98639b1b16798c7bec16d446aac
sporacid <sporacid@gmail.com>
2025-07-28 21:38:51 -0400

    chore: Update spore-codegen to 3.2.2

--- include/spore/meta.hpp
+++ include/spore/meta.hpp
@@ -2,5 +2,6 @@
 
 #include "spore/meta/meta.hpp"
 #include "spore/meta/meta_macros.hpp"
+#include "spore/meta/meta_string_convert.hpp"
 #include "spore/meta/meta_truthy.hpp"
 #include "spore/meta/meta_version.hpp"
