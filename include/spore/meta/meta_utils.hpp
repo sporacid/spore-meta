3ff0deb7b5f0be544ccb7ca17e8742d941103124
sporacid <sporacid@gmail.com>
2025-08-31 13:51:13 -0400

    fix: Simplify meta type id to work with string views

--- include/spore/meta/meta_utils.hpp
+++ include/spore/meta/meta_utils.hpp
@@ -1,36 +1,24 @@
 #pragma once
 
-#include <cstddef>
 #include <cstdint>
-
-#include "spore/meta/meta_string.hpp"
+#include <string_view>
 
 namespace spore::meta::utils
 {
-    namespace detail
+    constexpr std::uint64_t hash_string(const std::string_view string)
     {
-        template <std::size_t index_v, std::size_t capacity_v>
-        constexpr std::uint64_t hash_string_impl(const meta_string<capacity_v>& string, const std::uint64_t hash = 14695981039346656037ULL)
+        // From https://github.com/foonathan/string_id
+
+        constexpr std::uint64_t initial_hash = 14695981039346656037ULL;
+
+        std::uint64_t hash = initial_hash;
+
+        for (const char c : string)
         {
-            // From https://github.com/foonathan/string_id
-
-            if constexpr (index_v < meta_string<capacity_v>::size())
-            {
-                constexpr std::uint64_t fnv_prime = 1099511628211ULL;
-                const std::uint64_t c = static_cast<std::uint64_t>(string[index_v]);
-                const std::uint64_t new_hash = (hash ^ c) * fnv_prime;
-                return hash_string_impl<index_v + 1>(string, new_hash);
-            }
-            else
-            {
-                return hash;
-            }
+            constexpr std::uint64_t fnv_prime = 1099511628211ULL;
+            hash = (hash ^ static_cast<std::uint64_t>(c)) * fnv_prime;
         }
-    }
 
-    template <std::size_t capacity_v>
-    constexpr std::uint64_t hash_string(const meta_string<capacity_v>& string)
-    {
-        return detail::hash_string_impl<0>(string);
+        return hash;
     }
 }
