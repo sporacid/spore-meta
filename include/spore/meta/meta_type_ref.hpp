e0c7e836dd59889488981e5721bdfa34d0c5e506
sporacid <sporacid@gmail.com>
2025-10-20 10:15:33 -0400

    fix: Fix variadic argument type refs

--- include/spore/meta/meta_type_ref.hpp
+++ include/spore/meta/meta_type_ref.hpp
@@ -2,9 +2,17 @@
 
 namespace spore
 {
-    template <typename value_t>
+    template <typename... values_t>
     struct meta_type_ref
     {
+        using value_type = meta_tuple<values_t...>;
+        static constexpr bool variadic = true;
+    };
+
+    template <typename value_t>
+    struct meta_type_ref<value_t>
+    {
         using value_type = value_t;
+        static constexpr bool variadic = false;
     };
 }
